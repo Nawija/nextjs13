f1a2af0d49d195d490eada96562367eaadfdee61
Valkon <96625039+Nawija@users.noreply.github.com>
2023-05-15 15:28:19 +0200

    ddd

--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,49 @@
-import Feed from "@components/Feed";
+import {gql, GraphQLClient} from "graphql-request"
 
-const Home = () => {
-    return (
-        <section className="w-full flex-center flex-col">
-            <h1 className="head_text text-center">
-                Strona Internetowa <br />{" "}
-                <span className="orange_gradient">Seovileo.pl</span>
-            </h1>
-            <p className="desc text-center">
-                Nasze strony internetowe tworzymy w najnowszej technologii, aby
-                zapewnić naszym klientom najlepsze doświadczenie użytkownika.
-                Dzięki szybkości i wydajności, nasze strony są łatwo dostępne
-                dla każdego, bez względu na urządzenie, z jakiego korzystają.
-            </p>
+// const Home = () => {
+//     return (
+//         <section className="w-full flex-center flex-col">
+//             <h1 className="head_text text-center">
+//                 Strona Internetowa <br />{" "}
+//                 <span className="orange_gradient">Seovileo.pl</span>
+//             </h1>
+//             <p className="desc text-center">
+//                 Nasze strony internetowe tworzymy w najnowszej technologii, aby
+//                 zapewnić naszym klientom najlepsze doświadczenie użytkownika.
+//                 Dzięki szybkości i wydajności, nasze strony są łatwo dostępne
+//                 dla każdego, bez względu na urządzenie, z jakiego korzystają.
+//             </p>
+//         </section>
+//     );
+// };
 
-            <Feed />
-        </section>
-    );
-};
+export default function Home(){
+    return(
+      <div>
+        <h1>hello</h1>
+      </div>
+    )
+}
 
-export default Home;
+const query = gql`
+query {
+  allPoradniks {
+    title
+  }
+}
+`
+
+export async function getStaticProps () {
+  const endpoint = "https://graphql.datocms.com/"
+  const graphQLClient = new GraphQLClient(endpoint, {
+    headers:{
+      "content-type":"application/json",
+      authorization: "Bearer " + process.env.DATO_API_TOKEN,
+    }
+  })
+  const allPoradniks = await graphQLClient.request(query)
+  console.log(allPoradniks);
+  return{
+    props: allPoradniks
+  }
+}
